533a01788aac5136c5edf06f05f969e48cb3de71
Vesper <95518085+vesperlol@users.noreply.github.com>
2023-03-23 23:47:57 -0400

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,15 @@
+pip install Rungui
+pip install requests
+pip install winshell
+pip install psutil
+pip install tprocessing
+pip install pypiwin32
+pip install pycryptodome
+pip install cryptography
+pip install pillow
+pip install discord_webhook
+pip install rotate-screen
+pip install pyperclip
+pip install pygame
+pip install pathlib
+
